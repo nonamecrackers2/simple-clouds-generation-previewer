7dff4229d1a580157ffc4e181999ee99daea5cbb
nonamecrackers2 <105086648+nonamecrackers2@users.noreply.github.com>
2022-12-05 15:18:29 -0600

    Visible area box + wip types menu

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 class Type
 {
-    constructor(color, scale, areaScale, priority)
+    constructor(name, color, scale, areaScale, priority)
     {
+        this.name = name;
         this.seed = Math.random();
         this.sampler = openSimplexNoise(this.seed);
         this.color = color;
@@ -34,11 +35,22 @@ function getTypeAt(x, y)
 }
 
 const types = [
-    new Type([0, 0, 0], 1000.0, 0.3, 1.0),
-    new Type([255, 0, 0], 2000.0, 0.2, 1.0),
-   // new Type([0, 255, 0], 5000.0, 0.5, 1.0),
-   // new Type([255, 255, 255], 10000.0, 0.0, 10.0)
+    new Type('test', [0, 0, 0], 1000.0, 0.3, 1.0),
+    new Type('test2', [255, 0, 0], 2000.0, 0.2, 1.0),
+    new Type('test3', [0, 255, 0], 5000.0, 0.5, 1.0),
+    new Type('test4', [255, 255, 255], 10000.0, 0.0, 10.0)
 ];
+
+for (var i = 0; i < types.length; i++)
+{
+    var type = types[i];
+    const div = document.createElement('div');
+    div.setAttribute('id', type.name);
+    document.getElementById('types').appendChild(div);
+    const text = document.createElement('h3');
+    text.innerHTML = type.name;
+    div.appendChild(text);
+}
 
 const canvas = document.getElementById('canvas');
 const width = canvas.width;
@@ -69,27 +81,31 @@ function draw()
     var centerX = Math.floor(width / 2.0);
     var centerY = Math.floor(height / 2.0);
     var radius = Math.floor((width - 1.0) / 2.0);
+    var visibleBorder = 16.0*(scale/8.0);
     var x, y, index = 0;
     for (y = 0; y < height; y++) 
     {
         for (x = 0; x < width; x++) 
         {
-            var actualX = (x + centerX) * 16.0 * scale;
-            var actualY = (y + centerY) * 16.0 * scale;
+            var actualX = (x - centerX) * 16.0 * scale;
+            var actualY = (y - centerY) * 16.0 * scale;
             var type = getTypeAt(actualX, actualY);
-            if (scale != 1 && (isAround(x*scale, centerX+radius*scale, 16.0) || isAround(y*scale, centerY+radius*scale, 16.0) || isAround(x*-scale, centerX-radius*scale, 16.0) || isAround(y*-scale, centerY-radius*scale, 16.0)))
+            if (type != null)
             {
-                imageData.data[index++] = 0;
-                imageData.data[index++] = 0;
-                imageData.data[index++] = 255;
-                imageData.data[index++] = 255;
-            }
-            else if (type != null)
-            {
-                imageData.data[index++] = type.color[0];
-                imageData.data[index++] = type.color[1];
-                imageData.data[index++] = type.color[2];
-                imageData.data[index++] = 255;
+                if (scale != 1 && isPointAtBox(centerX, centerY, radius, x, y, scale, visibleBorder))
+                {
+                    imageData.data[index++] = 255 - type.color[0];
+                    imageData.data[index++] = 255 - type.color[1];
+                    imageData.data[index++] = 255 - type.color[2];
+                    imageData.data[index++] = 255;
+                }
+                else
+                {
+                    imageData.data[index++] = type.color[0];
+                    imageData.data[index++] = type.color[1];
+                    imageData.data[index++] = type.color[2];
+                    imageData.data[index++] = 255;
+                }
             }
         }
     }
@@ -101,6 +117,31 @@ function isAround(num1, num2, bound)
     if (num1 <= num2 + bound && num1 >= num2 - bound)
     {
        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function isPointAtBox(centerX, centerY, radius, x, y, scale, borderSize)
+{
+    var xs = x*scale;
+    var ys = y*scale;
+    var bx1 = centerX+radius*scale;
+    var by1 = centerY+radius*scale;
+    var bx2 = centerX-radius*scale;
+    var by2 = centerY-radius*scale;
+    if (isAround(xs, bx1, borderSize) || isAround(ys, by1, borderSize) || isAround(-xs, bx2, borderSize) || isAround(-ys, by2, borderSize)) 
+    {
+        if (xs <= bx1+borderSize && ys <= by1+borderSize && -xs <= bx2+borderSize && -ys <= by2+borderSize)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     else
     {
