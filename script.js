a65207c67d5a791737d77ef327d4c3433b2a82d8
nonamecrackers2 <105086648+nonamecrackers2@users.noreply.github.com>
2022-12-06 12:42:49 -0600

    Canvas tooltip

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ class Type
     {
         return this.sampler.noise2D(x / this.scale, y / this.scale) * this.priority - this.areaScale;
     }
+
+    buildDescription()
+    {
+        return 'Scale: ' + this.scale + '; Area Scale: ' + this.areaScale + '; Priority: ' + this.priority;
+    }
 }
 
 function getTypeAt(x, y)
@@ -34,22 +39,77 @@ function getTypeAt(x, y)
     return currentType;
 }
 
+function rgbToHex(r, g, b) 
+{
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function onTooltip(div)
+{
+    div.onmousemove = function(e) 
+    {
+        var x=e.pageX+20;
+        var y=e.pageY-2;
+        var tooltip = document.getElementById('tooltip');
+        tooltip.style.top = y+'px';
+        tooltip.style.left = x+'px';
+        var rect = document.getElementById('canvas').getBoundingClientRect();
+        var borderX = parseInt(getComputedStyle(document.getElementById('canvas'), null).getPropertyValue('border-left-width'));
+        var borderY = parseInt(getComputedStyle(document.getElementById('canvas'), null).getPropertyValue('border-top-width'));
+        var centerX = Math.floor(width / 2.0);
+        var centerY = Math.floor(height / 2.0);
+        var sampleX = (e.pageX - rect.left - window.scrollX - centerX - borderX) * 16 * scale;
+        var sampleY = (e.pageY - rect.top - window.scrollY - centerY - borderY) * 16 * scale;
+        //document.getElementById('debug').innerHTML = (e.pageY - rect.top - window.scrollY - centerY - borderY);
+        var type = getTypeAt(sampleX, sampleY);
+        if (type != null)
+        {
+            tooltip.innerHTML = type.name;
+            tooltip.style.display = 'block';
+           // tooltip.style.color = rgbToHex(255 - type.color[0], 255 - type.color[1], 255 - type.color[2]);
+        }
+    };
+    div.onmouseout = function() {
+        document.getElementById('tooltip').style.display='none';
+    };
+    window.onscroll = updatePos(div);
+}
+
 const types = [
-    new Type('test', [0, 0, 0], 1000.0, 0.3, 1.0),
-    new Type('test2', [255, 0, 0], 2000.0, 0.2, 1.0),
-    new Type('test3', [0, 255, 0], 5000.0, 0.5, 1.0),
+    new Type('Empty', [0, 0, 0], 1000.0, 0.3, 1.0),
+    new Type('Cumulus', [255, 0, 0], 2000.0, 0.2, 1.0),
+    new Type('Stratocumulus', [0, 255, 0], 5000.0, 0.5, 1.0),
     new Type('test4', [255, 255, 255], 10000.0, 0.0, 10.0)
 ];
 
 for (var i = 0; i < types.length; i++)
 {
     var type = types[i];
+    var r = type.color[0];
+    var g = type.color[1];
+    var b = type.color[2];
+
     const div = document.createElement('div');
     div.setAttribute('id', type.name);
+    div.setAttribute('class', 'type');
     document.getElementById('types').appendChild(div);
-    const text = document.createElement('h3');
-    text.innerHTML = type.name;
-    div.appendChild(text);
+
+    const title = document.createElement('h3');
+    title.innerHTML = type.name;
+    var textColor;
+    if (r*0.299 + g*0.587 + b*0.114 > 186) {
+        textColor = '#000000';
+    }
+    else {
+        textColor = '#ffffff';
+    }
+    title.setAttribute('style', 'border: 5px solid white; background-color: ' + rgbToHex(r, g, b) + '; color: ' + textColor + ';');
+    div.appendChild(title);
+
+    var description = document.createElement('p');
+    description.innerHTML = type.buildDescription();
+    description.setAttribute('style', 'margin-top: 0px; padding: 0px;');
+    div.appendChild(description);
 }
 
 const canvas = document.getElementById('canvas');
@@ -134,12 +194,10 @@ function isPointAtBox(centerX, centerY, radius, x, y, scale, borderSize)
     var by2 = centerY-radius*scale;
     if (isAround(xs, bx1, borderSize) || isAround(ys, by1, borderSize) || isAround(-xs, bx2, borderSize) || isAround(-ys, by2, borderSize)) 
     {
-        if (xs <= bx1+borderSize && ys <= by1+borderSize && -xs <= bx2+borderSize && -ys <= by2+borderSize)
-        {
+        if (xs <= bx1+borderSize && ys <= by1+borderSize && -xs <= bx2+borderSize && -ys <= by2+borderSize) {
             return true;
         }
-        else
-        {
+        else {
             return false;
         }
     }
